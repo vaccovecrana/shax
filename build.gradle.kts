884877587a004f0de5f78122f44d79041eb7e4bc
Jesus Zazueta <jjzazuet@vacco.io>
2020-09-18 16:46:19 -0400

    - Common build tweaks.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,30 @@
 buildscript {
   repositories {
+    jcenter(); gradlePluginPortal()
     maven { name = "VaccoOss"; setUrl("https://dl.bintray.com/vaccovecrana/vacco-oss") }
   }
-  dependencies { classpath("io.vacco.common:common-build:0.1.0") }
+  dependencies { classpath("io.vacco:common-build-gradle-plugin:0.5.0") }
 }
 
-apply(from = project.buildscript.classLoader.getResource("io/vacco/common/java-library.gradle.kts").toURI())
+apply{plugin(io.vacco.common.CbPlugin::class.java)}
 
 group = "io.vacco.shax"
-version = "1.7.30.0.0.4"
+version = "1.7.30.0.0.5"
+
+configure<io.vacco.common.CbPluginProfileExtension> {
+  addJ8Spec()
+  addPmd()
+  addSpotBugs()
+  addClasspathHell()
+  setPublishingUrlTransform { repo -> "${repo.url}/${project.name}" }
+  sharedLibrary()
+}
 
 configure<JavaPluginExtension> {
   sourceCompatibility = JavaVersion.VERSION_1_8
   targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-val implementation by configurations
-dependencies { implementation("org.slf4j:slf4j-api:1.7.30") }
+dependencies {
+  implementation("org.slf4j:slf4j-api:1.7.30")
+}
