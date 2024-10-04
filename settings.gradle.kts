628ba31a322bca03c04b1c87f676f780ce329625
Jesus Zazueta <jjzazuet@gmail.com>
2024-10-04 02:14:47 -0400

    - JUL logging output. - Environment log record parameter. - Log output tweaks. - Documentation update. - SLF4J api upgrade. - Build system upgrade. - Minor version bump.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,8 @@
-pluginManagement { repositories { mavenCentral(); gradlePluginPortal() } }
+pluginManagement {
+  repositories {
+    mavenCentral()
+    gradlePluginPortal()
+  }
+}
+
+rootProject.name = "shax"
